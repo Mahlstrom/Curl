1397beda67bb21c2b9434c69ebcc15e642e61f49
Mahlstrom <magnus.ahlstrom@sbsradio.se>
2014-08-02 13:35:11 +0200

    First

---
+++ src/CurlResponse.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @authors Jordan Hall <nukezilla@hotmail.co.uk> & JokerHacker
+ * @Acknowledge S.C. Chen <http://sourceforge.net/projects/simplehtmldom/>
+ * @Contributions Bennett Treptow <Upload class>
+ * @version 0.1
+ * @copyright 2012 SimplexPHP
+ */
+
+namespace mahlstrom\Curl;
+
+class CurlResponse {
+	public $data;
+	public $request;
+	public $info;
+	public $status_code;
+	public $header_list;
+
+	public function __construct() {
+		$this->data = '';
+		$this->info = array();
+		$this->status_code = 0;
+		$this->request = null;
+		$this->header_list = array();
+	}
+}
